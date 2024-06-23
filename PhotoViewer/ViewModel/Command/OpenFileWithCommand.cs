2961b5fd920062f6b31c5d53d2021d67a4737700
msl-ltd <msl-ltd@outlook.jp>
2024-06-23 09:35:37 +0900

    ファイルをプログラムから開くコマンドでモデルのファイル存在確認プロパティを参照する

--- PhotoViewer/ViewModel/Command/OpenFileWithCommand.cs
+++ PhotoViewer/ViewModel/Command/OpenFileWithCommand.cs
@@ -4,7 +4,6 @@
 // </copyright>
 // <author>MIYATA Tomoyuki</author>
 //-----------------------------------------------------------------------
-using Microsoft.VisualBasic.FileIO;
 using PhotoViewer.Mvvm;
 using System.Diagnostics;
 
@@ -13,7 +12,7 @@ namespace PhotoViewer.ViewModel.Command
     /// <summary>
     /// ファイルをプログラムから開くコマンドを定義します。
     /// </summary>
-    internal class OpenFileWithCommand : CommandBase<string>
+    internal class OpenFileWithCommand : CommandBase<MainViewModel>
     {
         /// <summary>
         /// 現在の状態でコマンドが実行可能かどうかを決定するメソッドを定義します。
@@ -23,7 +22,20 @@ namespace PhotoViewer.ViewModel.Command
         /// コマンドにデータを渡す必要がない場合は、このオブジェクトを null に設定できます。
         /// </param>
         /// <returns>このコマンドを実行できる場合は、true。それ以外の場合は、false。</returns>
-        protected override bool CanExecute(string parameter) => FileSystem.FileExists(parameter);
+        protected override bool CanExecute(MainViewModel parameter)
+        {
+            bool can = false;
+
+            if (parameter != null)
+            {
+                if (parameter.IsExist)
+                {
+                    can = true;
+                }
+            }
+
+            return can;
+        }
 
         /// <summary>
         /// コマンドが起動される際に呼び出すメソッドを定義します。
@@ -32,11 +44,11 @@ namespace PhotoViewer.ViewModel.Command
         /// コマンドにより使用されるデータです。
         /// コマンドにデータを渡す必要がない場合は、このオブジェクトを null に設定できます。
         /// </param>
-        protected override void Execute(string parameter)
+        protected override void Execute(MainViewModel parameter)
         {
-            if (FileSystem.FileExists(parameter))
+            if (parameter.IsExist)
             {
-                using (var process = Process.Start(parameter))
+                using (var process = Process.Start(parameter.FileName))
                 {
                 }
             }
