c039b8adee767a5f5b81a8ec9fe346f49cc44e8e
msl-ltd <msl-ltd@outlook.jp>
2024-08-17 09:56:23 +0900

    パッケージをExpression.Blend.Sdk.WPFからMicrosoft.Xaml.Behaviors.Wpfへ変更する

--- PhotoViewer/View/Behavior/TriggerActionBehaviorBase.cs
+++ PhotoViewer/View/Behavior/TriggerActionBehaviorBase.cs
@@ -6,7 +6,7 @@
 //-----------------------------------------------------------------------
 using System;
 using System.Windows;
-using System.Windows.Interactivity;
+using Microsoft.Xaml.Behaviors;
 
 namespace PhotoViewer.View.Behavior
 {
